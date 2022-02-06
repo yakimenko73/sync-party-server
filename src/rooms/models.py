b4eff2a373dc7b437010ac4cc9801eedd3bcdbf3
yakimenko73 <r.yakimenko.73@gmail.com>
2022-02-06 16:22:30 +0200

    rooms: set rooms key as primary key

--- src/rooms/models.py
+++ src/rooms/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class Room(models.Model):
-    key = models.CharField(max_length=settings.ROOM_KEY_LENGTH, unique=True)
+    key = models.CharField(max_length=settings.ROOM_KEY_LENGTH, unique=True, primary_key=True)
     public = models.BooleanField(default=False)
     host = models.CharField(max_length=32)
 
