95ce8ff847a83c2ec2cb5400464f09d2e9ee37cd
yakimenko73 <r.yakimenko.73@gmail.com>
2022-02-06 12:45:35 +0200

    session: change middleware

--- src/config/settings.py
+++ src/config/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'sessions.middleware.CookielessSessionMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
