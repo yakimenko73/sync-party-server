befaa0a9dccf2a3ea25c28ec7b30c20b78e408a1
yakimenko73 <r.yakimenko.73@gmail.com>
2022-02-18 13:59:54 +0200

    cache: remove timeout for user data

--- src/cache/user.py
+++ src/cache/user.py
@@ -7,10 +7,10 @@ from django.core.cache import cache
 def init_usernames_cache():
     with open(f"{settings.STATIC_ROOT}/userNames.json", "r") as f:
         usernames = json.load(f)
-        cache.set(settings.USERNAMES_CACHE_NAME, usernames["names"])
+        cache.set(settings.USERNAMES_CACHE_NAME, usernames["names"], timeout=None)
 
 
 def init_user_colors_cache():
     with open(f"{settings.STATIC_ROOT}/userColors.json", "r") as f:
         colors = json.load(f)
-        cache.set(settings.USER_COLORS_CACHE_NAME, list(colors.values()))
+        cache.set(settings.USER_COLORS_CACHE_NAME, list(colors.values()), timeout=None)
